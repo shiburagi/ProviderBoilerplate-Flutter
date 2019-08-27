919205a0586f6c13a3cc4d9e56485e19cdd8d26d
Shiburagi <tr32010@gmail.com>
2019-08-27 11:54:50 +0800

    add snackbar component

---
+++ lib/components/snackbar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+makeSnackbar(
+        {@required BuildContext context,
+        String text,
+        SnackbarType type = SnackbarType.success}) =>
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text(text),
+      backgroundColor: snackbarBackgroundColor(context)[type],
+    ));
+
+enum SnackbarType {
+  normal,
+  success,
+  info,
+  danger,
+  warning,
+}
+
+Map<SnackbarType, Color> snackbarBackgroundColor(BuildContext context) => {
+      SnackbarType.normal: Theme.of(context).cardColor,
+      SnackbarType.success: Colors.green,
+      SnackbarType.info: Theme.of(context).accentColor,
+      SnackbarType.danger: Theme.of(context).errorColor,
+      SnackbarType.warning: Colors.yellow,
+    };
+
+Map<SnackbarType, Color> snackbarForegroundColor(BuildContext context) => {
+      SnackbarType.normal: Theme.of(context).textTheme.body1.color,
+      SnackbarType.success: Colors.white,
+      SnackbarType.success: Colors.white,
+      SnackbarType.info: Colors.white,
+      SnackbarType.danger: Colors.white,
+      SnackbarType.warning: Colors.black,
+    };
