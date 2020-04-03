64f9048847b98a330d03ac95dad3590e2208d42e
Shiburagi <tr32010@gmail.com>
2020-04-03 21:04:38 +0800

    provider listen : false

--- lib/bloc/bloc_state.dart
+++ lib/bloc/bloc_state.dart
@@ -2,5 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:provider_boilerplate/provider_boilerplate.dart';
 
 abstract class BlocState<T extends StatefulWidget, T2 extends BaseBloc> extends State<T>{
-    T2 get bloc => Provider.of(context)..register(context);
+    T2 get bloc => Provider.of(context, listen: false);
+
+    @override
+    void didChangeDependencies() {
+      super.didChangeDependencies();
+      bloc.register(context);
+    }
 }
